5c421f989c15a0f6293da7355dd2e198c81d3bd0
shahriaraf <shahriaraf01@gamil.com>
2025-01-28 22:33:17 +0600

    added files

--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -1,9 +1,11 @@
-// import { Navigate } from 'react-router-dom';
-// import { useAuth } from './Provider/Authprovider'; // Example Auth hook
+import { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import { AuthContext } from './Provider/Authprovider';
 
-// const PrivateRoute = ({ children }) => {
-//   const { user } = useAuth(); // Check user authentication
-//   return user ? children : <Navigate to="/login" replace />;
-// };
 
-// export default PrivateRoute;
+const PrivateRoute = ({ children }) => {
+  const { user } = useContext(AuthContext); // Check user authentication
+  return user ? children : <Navigate to="/login" replace />;
+};
+
+export default PrivateRoute;
