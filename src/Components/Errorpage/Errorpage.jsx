1cac1124fe95696a691712ed905900dd7d9e4825
shahriaraf <shahriaraf01@gamil.com>
2025-01-28 22:31:41 +0600

    updated error page

--- src/Components/Errorpage/Errorpage.jsx
+++ src/Components/Errorpage/Errorpage.jsx
@@ -4,7 +4,7 @@ const Errorpage = () => {
     return (
         <div className="text-center mt-20">
           <h1 className="text-4xl text-red-500">404 - Page Not Found</h1>
-          <p className="text-lg mt-2">The page youre looking for doesnadt exist.</p>
+          <p className="text-lg mt-2">The page youre looking for dosen't exist.</p>
         </div>
       );
 };
