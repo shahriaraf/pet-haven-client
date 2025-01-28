ba03b0b639004f5083fa6e0ee11a470d00783b55
shahriaraf <shahriaraf01@gamil.com>
2025-01-28 22:26:59 +0600

    added files

--- src/Components/Hooks/AxiosSecure.jsx
+++ src/Components/Hooks/AxiosSecure.jsx
@@ -10,7 +10,7 @@ const axiosSecure = axios.create({
 const AxiosSecure = () => {
     const navigate = useNavigate();
     const {signOutUser} = useContext(AuthContext);
-    axiosSecure.interceptors.request.use(function (config) {
+    axiosSecure.interceptors.request.use(function (config) { 
         const token = localStorage.getItem('access-token')
         console.log('request stopped by interceptors', token)
         config.headers.authorization = `Bearer ${token}`;
