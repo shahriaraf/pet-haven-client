fcadea38e7409a2b14a3165226e8d0196980ac2d
shahriaraf <shahriaraf01@gamil.com>
2025-02-19 16:36:18 +0600

    added files

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -17,10 +17,10 @@ const Footer = () => {
                                 <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
                                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
                                     <li className="mb-4">
-                                        <a href="https://flowbite.com/" className="hover:underline">Flowbite</a>
+                                        <a href="https://flowbite.com/" className="hover:underline">Services</a>
                                     </li>
                                     <li>
-                                        <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
+                                        <a href="https://tailwindcss.com/" className="hover:underline">Contact</a>
                                     </li>
                                 </ul>
                             </div>
@@ -51,7 +51,7 @@ const Footer = () => {
                     <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                     <div className="sm:flex sm:items-center sm:justify-between">
                         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                            © 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+                            © 2023 <a href="https://flowbite.com/" className="hover:underline">PetHaven™</a>. All Rights Reserved.
                         </span>
                         <div className="flex mt-4 sm:justify-center sm:mt-0">
                             <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
