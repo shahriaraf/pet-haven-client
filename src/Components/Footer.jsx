23f97b27b2f48cde11313a57a92b7fda26c9911a
shahriaraf <shahriaraf01@gamil.com>
2025-02-19 16:37:17 +0600

    added files

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -36,6 +36,9 @@ const Footer = () => {
                                 </ul>
                             </div>
                             <div>
+
+
+                                
                                 <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
                                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
                                     <li className="mb-4">
